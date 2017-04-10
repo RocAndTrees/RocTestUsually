7c7672f20ff95ff8b1feb2ef2a0d76ec4d00149b
linxiaopeng <359539865@qq.com>
2017-04-10 19:52:58 +0800

    adjust sturcte list about SwiftDemo

--- RocUsuallyObject-C/Pods/Baidu-Maps-iOS-SDK/BaiduMapAPI_Base.framework/Headers/BMKMapManager.h
+++ RocUsuallyObject-C/Pods/Baidu-Maps-iOS-SDK/BaiduMapAPI_Base.framework/Headers/BMKMapManager.h
@@ -14,7 +14,6 @@ enum EN_PERMISSION_STATUS
 	E_PERMISSION_OK = 0,	// 授权验证通过
 	E_PERMISSION_SERVER_ERROR = -200, //服务端数据错误，无法解析服务端返回数据
 	E_PERMISSION_NETWORK_ERROR = -300, //无法建立与服务端的连接
-    
 };
 
 //地图模块枚举
